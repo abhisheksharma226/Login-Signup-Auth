034dee347b904983ddbeedd463cd51cb0246ed0e
Abhishek Sharma <avisheksharma2004@gmail.com>
2024-02-16 23:39:42 +0530

    Server Started

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+
+const app = express();
+const PORT = 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT , () => {
+    console.log(`Server Started at ${PORT}`);
+})
